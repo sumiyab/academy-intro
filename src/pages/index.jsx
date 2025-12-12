e1ef5ff02e64fa82cd7eee744bc16b6ed911a0fa
sumiyab <sumiyabazaramarjargal@gmail.com>
2025-12-12 11:45:45 +0800

    change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,9 +7,6 @@ const rubik = Rubik_Mono_One({
 const Home = () => {
   return (
     <div>
-      <div className="text-3f-gg bg-background p-8 hover:text-hover max-sm:bg-amber-600 max-sm:flex max-sm:flex-col">
-        hello world
-      </div>
       <div className={rubik.className}>TOM</div>
     </div>
   );
